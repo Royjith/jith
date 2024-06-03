5ec914fc22330cea44c24c8147fbc79b4d1d20f4
Royjith <153445207+Royjith@users.noreply.github.com>
2024-06-03 18:01:00 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,63 @@
+provider "vsphere" {
+  user 			= "satyajith"
+  password		= "Welcome@123"
+  vsphere_server	= "172.28.8.5"
+  allow_unverified_ssl  = true
+}
+data "vsphere_datacenter" "datacenter" {
+  name = "T-Hub"
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = "Local-2.1"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = "T-Hub"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_network" "network" {
+  name          = "VM Network"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = "Terraform"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = "sonarqube"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  num_cpus         = 1
+  memory           = 1024
+  guest_id         = data.vsphere_virtual_machine.template.guest_id
+  scsi_type        = data.vsphere_virtual_machine.template.scsi_type
+  network_interface {
+    network_id   = data.vsphere_network.network.id
+    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+  }
+  disk {
+    label            = "disk0"
+    size             = data.vsphere_virtual_machine.template.disks.0.size
+    thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+  }
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+    customize {
+      linux_options {
+        host_name = "SonarQube"
+        domain    = "example.com"
+      }
+      network_interface {
+        ipv4_address = "172.28.8.8"
+        ipv4_netmask = 24
+      }
+      ipv4_gateway = "172.28.8.1"
+    }
+  }
+}
+
